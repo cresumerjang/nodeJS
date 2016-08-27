e7d8d2baac8fdfcf070efd64f0bae82a5cdd34fb
JANG JAE WON <cresumerjang@gmail.com>
2016-08-27 13:19:02 +0900

    add static middleware for serving static resources

--- express/webserver.js
+++ express/webserver.js
@@ -1,12 +1,27 @@
 var express = require('express'),
     app = express();
 
-app.listen(3000, function(){
-    console.log('connected port 3000');
-    console.log(__dirname);
-});
+// var appConfig = {
+//     port : 3000,
+//     staticPath : 'static',
+//     controller : {
+//         main : '/',
+//         page : '/page',
+//         list : '/list',
+//         detail : '/detail'
+//     }
+// };
+
+app
+    // 스태틱 리소스 root dir 지정
+    .use(express.static('statice'))
+    .listen(3000, function(){
+        console.log('connected port 3000');
+    });
 
 // 리퀘스트 맵핑
-app.get('/', function(req, res){
-    res.send('200');
-});
+app
+    // main
+    .get('/', function(req, res){
+        res.send('200');
+    });
